d2b6fed0f9daf5376add923e94047b28e9cbae1d
Devadathan S <devadathan.s.s92@kalvium.community>
2026-09-20 17:15:45 +0530

    Restore client application and add gitignore

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import BeneficiaryCard from "./components/BeneficiaryCard";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <BeneficiaryCard />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+function Sidebar() {
+  return (
+    <aside>
+      <h2>Menu</h2>
+
+      <ul>
+        <li>Dashboard</li>
+        <li>Beneficiaries</li>
+        <li>Reports</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
